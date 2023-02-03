9a83264a6963716517a3f74174fb2861474ab71d
Obee123 <aobiaderi@yahoo.com>
2023-02-03 20:47:20 +0000

    declared variables in js file

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+// variables to select html elements
+const searchForm = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const todayContainer = document.getElementById("today");
+const forecastContainer = document.getElementById("forecast");
+const historyContainer = document.getElementById("history");
+
