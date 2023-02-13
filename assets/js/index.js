03d350127ef6d0a79342e31897891158d5c5992c
Obee123 <aobiaderi@yahoo.com>
2023-02-13 16:23:49 +0000

    declared variables to call weather API and created function to display weather forecast for today

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,29 @@ var APIKey = "fa766ad2df19558f80ac998a4f5cbf45";
 searchForm.submit(function(e) {
   e.preventDefault();
   // Get the city name from the input
-  const city = searchInput.val();})
-    
+  const city = searchInput.val();
 
+  // Build the URLs to query the database
+  var currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+  var forecastWeatherURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey;
+
+  // Make an AJAX request to the weather API to get the current weather data
+  $.ajax({
+    url: currentWeatherURL,
+    type: "GET",
+    dataType: "json",
+    success: function(data) {
+      
+      // Handle the current weather data
+      var date = new Date();
+      var dateString = date.toLocaleDateString();
+      
+      // Convert the temp to Celsius
+      var tempC = data.main.temp - 273.15;
+
+      // add weather contents to html
+      todayContainer.html('<h4>' + data.name +" (" + dateString + ")"+ '<img src="https://openweathermap.org/img/w/' + data.weather[0].icon + '.png" alt="Weather icon">' +'</h4>  <p>Temp: ' + tempC.toFixed(2) + '°C' + '<p>Wind: ' + data.wind.speed + 'KPH' + '</p> <p>Humidity: ' + data.main.humidity + '%'+ '</p> </div>');
+    }
+  });
+    
+})
